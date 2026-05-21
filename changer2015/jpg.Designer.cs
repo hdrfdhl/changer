8ba313b05fe6d9153bf9113c968a84ff380eda14
riyanzackisaputra-png <riyanzacki.saputra@gmail.com>
2026-05-21 12:57:04 +0700

    Jpg design

---
+++ changer2015/jpg.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace changer2015
+{
+    partial class jpg
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(jpg));
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btndownload = new System.Windows.Forms.Button();
+            this.btnconvert = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbto = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbfrom = new System.Windows.Forms.ComboBox();
+            this.btnback = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.btnpf = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(1047, 348);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(33, 36);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 27;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(904, 422);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(57, 62);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 26;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btndownload
+            // 
+            this.btndownload.Location = new System.Drawing.Point(864, 609);
+            this.btndownload.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btndownload.Name = "btndownload";
+            this.btndownload.Size = new System.Drawing.Size(134, 36);
+            this.btndownload.TabIndex = 25;
+            this.btndownload.Text = "Download";
+            this.btndownload.UseVisualStyleBackColor = true;
+            // 
+            // btnconvert
+            // 
+            this.btnconvert.Location = new System.Drawing.Point(1063, 436);
+            this.btnconvert.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnconvert.Name = "btnconvert";
+            this.btnconvert.Size = new System.Drawing.Size(108, 36);
+            this.btnconvert.TabIndex = 24;
+            this.btnconvert.Text = "Convert";
+            this.btnconvert.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Javanese Text", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(778, 519);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 47);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "To:";
+            // 
+            // cmbto
+            // 
+            this.cmbto.FormattingEnabled = true;
+            this.cmbto.Items.AddRange(new object[] {
+            "PDF",
+            "Word"});
+            this.cmbto.Location = new System.Drawing.Point(839, 526);
+            this.cmbto.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cmbto.Name = "cmbto";
+            this.cmbto.Size = new System.Drawing.Size(188, 28);
+            this.cmbto.TabIndex = 22;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Javanese Text", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(756, 342);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 47);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "From:";
+            // 
+            // cmbfrom
+            // 
+            this.cmbfrom.FormattingEnabled = true;
+            this.cmbfrom.Items.AddRange(new object[] {
+            "PDF",
+            "Word"});
+            this.cmbfrom.Location = new System.Drawing.Point(839, 348);
+            this.cmbfrom.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cmbfrom.Name = "cmbfrom";
+            this.cmbfrom.Size = new System.Drawing.Size(188, 28);
+            this.cmbfrom.TabIndex = 20;
+            // 
+            // btnback
+            // 
+            this.btnback.Location = new System.Drawing.Point(1353, 258);
+            this.btnback.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnback.Name = "btnback";
+            this.btnback.Size = new System.Drawing.Size(108, 36);
+            this.btnback.TabIndex = 19;
+            this.btnback.Text = "Kembali";
+            this.btnback.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bookman Old Style", 28.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(551, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(456, 66);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "JPG CHANGER";
+            // 
+            // panel1
+            // 
+            this.panel1.AllowDrop = true;
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.webBrowser1);
+            this.panel1.Location = new System.Drawing.Point(55, 192);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(586, 651);
+            this.panel1.TabIndex = 17;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(236, 315);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 21);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Drop Here";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(22, 25);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(586, 651);
+            this.webBrowser1.TabIndex = 1;
+            this.webBrowser1.Visible = false;
+            // 
+            // btnpf
+            // 
+            this.btnpf.Location = new System.Drawing.Point(1219, 258);
+            this.btnpf.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnpf.Name = "btnpf";
+            this.btnpf.Size = new System.Drawing.Size(108, 36);
+            this.btnpf.TabIndex = 16;
+            this.btnpf.Text = "Pilih File";
+            this.btnpf.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(839, 262);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(352, 26);
+            this.textBox1.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Javanese Text", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(714, 256);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 47);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Nama File:";
+            // 
+            // jpg
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(1516, 901);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btndownload);
+            this.Controls.Add(this.btnconvert);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cmbto);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cmbfrom);
+            this.Controls.Add(this.btnback);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnpf);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "jpg";
+            this.Text = "jpg";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btndownload;
+        private System.Windows.Forms.Button btnconvert;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbto;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbfrom;
+        private System.Windows.Forms.Button btnback;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button btnpf;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
